cb7533971437080b07482e870ccbf7bdd33d0c47
Darryl Davis <ddavis1306@users.noreply.github.com>
2022-10-16 20:40:24 -0700

    listsNlists

---
+++ input/makeName.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env Python3
+#An input asking the user's name.
+#An input asking what day of the week it is.
+#A print statement that reads:Hello+ <name>! Happy <day of the week>!
+
+
+def main():
+    name_is = input("Whats your name? ")
+    day_is = input("Hi, " + name_is + "! " + "What day of the week is it? ")
+    print("Hello, " + name_is + " Happy " + day_is + "!")
+main()
+zlearn = "this is my string"
+print(zlearn + " and this is my thing")
---
+++ intLabs/strings.py
@@ -0,0 +1,22 @@
+#!/bin/env/ Python3
+
+mystring = "Hi my name is"
+picky = "picky picky"
+slim = "slim SHADY"
+
+print(mystring + " HUHhh " + "my name is what?? " + "my name is " + picky + " " + slim)
+#print(slim[5:9])
+
+echo = " echo ECHO"
+
+print(echo * 5)
+
+cave = "drip.drop.drip.drop.drip.drop.drip.drop.drip.drop"
+
+print(cave.split('.'))
+
+spacedCave = "          TONS OF SPACE HERE"
+print(spacedCave)
+print(spacedCave.strip().lower())
+print(spacedCave.startswith(" "))
+
