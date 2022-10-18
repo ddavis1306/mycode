fbaed3eed975c4cafbdb141a4f34e419b2438dec
Darryl D <ddavis1306@users.noreply.github.com>
2022-10-18 04:17:35 +0000

    added 19

---
+++ Lab19/mix_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" lab 16 project """
+
+my_list = ["127.32.22.1", 5060, "up"]
+plist = []
+print(f"first item in the list (IP): {my_list[0]}")
+print(f"second item in the list (PORT): {my_list[1]}")
+print(f"third item in the list (state): {str(my_list[2])}")
